1d5a80326954e3550676e6b8812378bfc652daeb
RealityAdmin <soulmind.os@gmail.com>
2026-05-01 15:10:51 -0400

    Completing chapter 2 of book, initial service scaffold complete

---
+++ movie/cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"movieexamplekhubaib.com/movie/internal/controller/movie"
+	metadatagateway "movieexamplekhubaib.com/movie/internal/gateway/metadata/http"
+	ratinggateway "movieexamplekhubaib.com/movie/internal/gateway/rating/http"
+	httphandler "movieexamplekhubaib.com/movie/internal/handler/http"
+)
+
+func main() {
+	log.Println("Starting movie service")
+	metadataGateway := metadatagateway.New("localhost:8081")
+	ratingGateway := ratinggateway.New("localhost:8082")
+	ctrl := movie.New(ratingGateway, metadataGateway)
+	h := httphandler.New(ctrl)
+	http.Handle("/movie", http.HandlerFunc(h.GetMovieDetails))
+	if err := http.ListenAndServe(":8083", nil); err != nil {
+		panic(err)
+	}
+}
--- movie/pkg/model/model.go
+++ movie/pkg/model/model.go
@@ -3,6 +3,6 @@ package model
 import "movieexamplekhubaib.com/metadata/pkg/model"
 
 type MovieDetails struct {
-	Rating   *float64       `json:"rating,omitEmpty"`
+	Rating   *float64       `json:"rating,omitempty"`
 	Metadata model.Metadata `json:"metadata"`
 }
